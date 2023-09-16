2755a095eff7163b4160066c63762b845e2981d2
OGADAMo <adam.marjanovic2@gmail.com>
2023-09-16 17:52:33 +0200

    Session display changes

--- app/classes/User.php
+++ app/classes/User.php
@@ -18,7 +18,7 @@ class User {
         $result = $stmt->execute();
 
         if ($result) {
-            $_SESSION['user_id'] = $result->insert_id;
+            $_SESSION['user_id'] = $result;
             return true;
         } else {
             return false;
