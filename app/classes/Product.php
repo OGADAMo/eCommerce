89c49f9d476f88fb234592639250c7666a61c790
OGADAMo <adam.marjanovic2@gmail.com>
2023-09-18 21:19:22 +0200

    List all Products

--- app/classes/Product.php
+++ app/classes/Product.php
@@ -1,10 +1,19 @@
 <?php
+
+use LDAP\Result;
+
     class Product {
         protected $conn;
     
         public function __construct() {
             global $conn;
             $this->conn = $conn;
+        }
+
+        public function fetch_all() {
+            $sql = "SELECT * FROM products";
+            $result = $this->conn->query($sql);
+            return $result->fetch_all(MYSQLI_ASSOC);
         }
 
     }
--- index.php
+++ index.php
@@ -1,15 +1,30 @@
-<?php require_once 'inc/header.php' ?>
+<?php
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+require_once 'inc/header.php';
+require_once 'app/classes/Product.php';
+
+$products = new Product();
+$products = $products->fetch_all();
+
+?>
+
+<div class="row">
+    <?php foreach ($products as $product): ?>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="<?php echo $product['image']; ?>" class="card-img-top" alt="Product Image">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $product['name']; ?></h5>
+                    <p class="card-text">
+                        Price: $<?php echo number_format($product['price'], 2); ?><br>
+                        Size: <?php echo $product['size']; ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
+
+
 
 <?php require_once 'inc/footer.php' ?>
