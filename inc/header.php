058001dfdae91e2f884475f506efae806d6e7746
OGADAMo <adam.marjanovic2@gmail.com>
2023-09-20 10:41:41 +0200

    Add product images and display product

--- inc/header.php
+++ inc/header.php
@@ -13,52 +13,50 @@ $user = new User();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="public\css\style.css" type="text/css">
     <title>Shaghi Shop</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
-            <div class="container">
-                <a class="navbar-brand" href="index.php">Shaghi Shop</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="index.php">Home</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Shaghi Shop</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <?php if(!$user->is_logged()) : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register.php">Register</a>
                         </li>
-                    </ul>
-                    <ul class="navbar-nav ml-auto">
-                        <?php if(!$user->is_logged()) : ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="register.php">Register</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="login.php">Login</a>
-                            </li>
-                        <?php else: ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="my-orders.php">My Orders</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="logout.php">Logout</a>
-                            </li>
-                        <?php endif; ?>
-                    </ul>
-                    
-                </div>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                    <?php else: ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="my-orders.php">My Orders</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">Logout</a>
+                        </li>
+                    <?php endif; ?>
+                </ul>   
             </div>
-        </nav>
+        </div>
+    </nav>
     <div class="container">
         <?php if (isset($_SESSION['message'])) : ?>
-                <div class="alert alert-<?php  echo $_SESSION['message']['type']; ?> alert-dismissible fade show" role="alert">
-                    <?php 
-                        echo $_SESSION['message']['text']; 
-                        unset($_SESSION['message'])
-                    ?>
-        
-                </div>
-        
+            <div class="alert alert-<?php  echo $_SESSION['message']['type']; ?> alert-dismissible fade show" role="alert">
+                <?php 
+                    echo $_SESSION['message']['text']; 
+                    unset($_SESSION['message'])
+                ?>
+            </div>
         <?php endif; ?>
 
         
--- product.php
+++ product.php
@@ -5,7 +5,22 @@ require_once 'app/classes/Product.php';
 $product = new  Product();
 $product= $product->read($_GET['product_id']);
 
-echo $_GET['product_id'];
-var_dump($product);
-
 ?>
+
+<div class="row">
+    <div class="col-lg-6">
+        <img src="<?php echo $product['image']; ?>" alt="img-fluid" class="product-image">
+    </div>
+
+    <div class="col-lg-6">
+        <h2><?php echo $product['name']; ?></h2>
+        <p>Price: $<?php echo number_format($product['price'], 2); ?></p>
+        <p>Size: <?php echo $product['size']; ?></p>
+        <form action="" method="POST">
+            <button type="submit" class="btn btn-primary">Add to Cart</button>
+        </form>
+    </div>
+</div>
+
+
+<?php require_once 'inc/footer.php'; ?>
